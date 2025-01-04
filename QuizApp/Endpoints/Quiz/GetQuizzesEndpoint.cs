69af6b78740d644b4d8c22c9036bfb58fe8a9d48
Davis Hoang <davishoang96@gmail.com>
2025-01-04 19:15:13 +0800

    init

---
+++ QuizApp/Endpoints/Quiz/GetQuizzesEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using FastEndpoints;
+using QuizApp.Common.DTO;
+using QuizApp.Database.Repositories;
+
+namespace QuizApp.Endpoints.Quiz;
+
+public class GetQuizzesEndpoint : Endpoint<EmptyRequest, IEnumerable<QuizDTO>>
+{
+    private readonly IQuizRepository quizRepository;
+    public GetQuizzesEndpoint(IQuizRepository quizRepository)
+    {
+        this.quizRepository = quizRepository;
+    }
+
+    public override void Configure()
+    {
+        Get("quiz/getquizzes");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(EmptyRequest r, CancellationToken ct)
+    {
+        var result = await quizRepository.GetAllQuizzes();
+        if (result == null)
+        {
+            ThrowError("Cannot get all quizzes");
+        }
+
+        await SendAsync(result);
+    }
+}
