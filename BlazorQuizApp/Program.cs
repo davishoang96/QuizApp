8d90153f9ebcc37dd2e04d7b50848ce0f74ebd7c
Davis Hoang <davishoang96@gmail.com>
2025-01-06 12:09:59 +0800

    migrate database contextr

--- BlazorQuizApp/Program.cs
+++ BlazorQuizApp/Program.cs
@@ -1,7 +1,20 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using BlazorQuizApp.Client.Pages;
 using BlazorQuizApp.Components;
+using Microsoft.EntityFrameworkCore;
+using QuizApp.Database;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+        .ConfigureContainer<ContainerBuilder>(builder =>
+        {
+            builder.RegisterModule(new RepositoryModule());
+        });
+
+var dataSource = builder.Configuration["DataSource"];
+builder.Services.AddDbContext<QuizContext>(options =>
+    options.UseSqlite($"Data Source={dataSource}"));
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -9,6 +22,12 @@ builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
 
 var app = builder.Build();
+
+// Apply migration
+using (var scope = ((IApplicationBuilder)app).ApplicationServices.GetService<IServiceScopeFactory>()!.CreateScope())
+{
+    scope.ServiceProvider.GetRequiredService<QuizContext>().Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
